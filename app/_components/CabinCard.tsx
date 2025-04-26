acb7280fad391dc1527083454a24e4a104687347
Gabriel Kassarp <gkassarp@gmail.com>
2025-04-26 20:03:50 +0200

    added same contitional rendering for cabincard

--- app/_components/CabinCard.tsx
+++ app/_components/CabinCard.tsx
@@ -9,12 +9,14 @@ function CabinCard({ cabin } : { cabin: cabinInterface }) {
   return (
     <div className="flex border-primary-800 border">
       <div className="flex-1 relative">
-        <Image
-          src={image}
-          fill
-          alt={`Cabin ${name}`}
-          className="object-cover border-r border-primary-800"
-        />
+       {image && (
+         <Image
+           src={image}
+           fill
+           alt={`Cabin ${name}`}
+           className="object-cover border-r border-primary-800"
+         />
+        )}
       </div>
 
       <div className="flex-grow">
